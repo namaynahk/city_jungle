68a0fbaf41c39d39e2b52ae41bcbaf33fc851fa0
Yaman <yaman@ubuntu.ubuntu-domain>
2012-08-17 01:41:29 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CityJungle::Application.config.secret_token = 'cbd51edb2771df46ee82daf80a6c6658a8bfe2d2ace6c0030858d44de1b4342137eafdacd314274f9468721ee27fd4f0d8ada13231b79a95ebe5dcb87a0b6d89'
